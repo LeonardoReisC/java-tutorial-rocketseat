68982a4cdb6ad4bc6f123b3099abe13dbc601960
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2023-10-11 18:04:14 -0300

    integrates with H2 database

--- todolist/src/main/java/br/com/coimbralreis/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/coimbralreis/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.coimbralreis.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody User user) {
-        System.out.println(user.getName());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            System.out.println("User already exists!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists!");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
